337435eff3bb0df88d47214328a5d5de42a10994
insane-nomad <daglibdaglib@gmail.com>
2023-01-14 21:09:36 +0300

    add analyze func

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
-func main() {
+type data struct {
+	word  string
+	count int
+}
 
-	bytes, err := os.ReadFile("text.txt")
+func readFile(fileName string) string {
+	bytes, err := os.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err)
 	}
-	text := string(bytes)
-	replacer := strings.NewReplacer("’", "", ",", "", ".", "", "”", "", "“", "", "?", "", "!", "")
-	text = replacer.Replace(text)
+	return string(bytes)
+}
+
+func analyze(text string) {
+	text = strings.ReplaceAll(text, "\n", " ")
+	text = strings.ReplaceAll(text, "\r", " ")
+	regexp, err := regexp.Compile(`[^\w]`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	text = regexp.ReplaceAllString(text, " ")
+	//split := strings.Split(text, " ")
+	split := strings.Fields(text)
+	fmt.Println(split)
+}
+
+func main() {
+
+	text := readFile("text.txt")
+	analyze(text)
+
 	fmt.Println(text)
 }
